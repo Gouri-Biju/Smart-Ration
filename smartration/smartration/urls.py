876d4a5dc4f851fe14620dbbaeecb3a7928664d0
Gouri-Biju <gouribiju24@gmail.com>
2025-09-06 06:12:53 +0530

    urls edited

--- smartration/smartration/urls.py
+++ smartration/smartration/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('smartrationapp.urls')),
+    path('',include('smartration.smartrationapp.urls')),
 ]
