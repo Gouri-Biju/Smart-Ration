04830fe1ffa3c9b87aa703c6e20929d70c9fd738
Gouri-Biju <gouribiju24@gmail.com>
2025-09-06 07:19:29 +0530

    template updated

--- smartration/smartration/settings.py
+++ smartration/smartration/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'smartration.smartration.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')], 
+        'DIRS': [ 'smartration.templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
